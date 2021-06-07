c308197ae2dabb7385315f9a1b708df639c2e502
Karina Ayala <karinaayala2021@u.northwestern.edu>
2021-06-06 23:20:37 -0500

    fixed small bug in models/index.js

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ Product.belongsTo(Category, {
   foreignKey: 'category_id'
 });
 // Categories have many Products
-Category.hadMany(Product, {
+Category.hasMany(Product, {
   foreignKey: 'category_id',
   onDelete: 'CASCADE'
 });
